93700942116f13141fc136e1a6d1322f71e1f4aa
Matija Pretnar <matija@pretnar.info>
2011-06-20 11:11:05 +0200

    Maybe settings will work now.

--- settings-dev.py
+++ settings-dev.py
@@ -4,6 +4,30 @@ from tomo.settings import *
 
 # Production settings
 
+# http://www.djangosnippets.org/snippets/501/
+### ACTIVE DIRECTORY SETTINGS
+
+# AD_DNS_NAME should set to the AD DNS name of the domain (ie; example.com)  
+# If you are not using the AD server as your DNS, it can also be set to 
+# FQDN or IP of the AD server.
+
+AD_DNS_NAME = 'warpout.fmf.uni-lj.si'
+AD_LDAP_PORT = 389
+
+AD_SEARCH_DN = 'ou=uporabniki,dc=std,dc=fmf,dc=uni-lj,dc=si'
+
+# This is the NT4/Samba domain name
+AD_NT4_DOMAIN = 'std'
+
+AD_SEARCH_FIELDS = ['mail','givenName','sn','sAMAccountName']
+
+AD_LDAP_URL = 'ldap://%s:%s' % (AD_DNS_NAME,AD_LDAP_PORT)
+
+AUTHENTICATION_BACKENDS = (
+    'tomo.auth.ActiveDirectoryBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
