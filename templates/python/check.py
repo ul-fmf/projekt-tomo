afa9435eed89abf0f3f55f0cfaba4b668cee6dba
Matija Pretnar <matija@pretnar.info>
2011-10-20 08:58:08 +0200

    More fixes.

--- templates/python/check.py
+++ templates/python/check.py
@@ -36,7 +36,9 @@ class Check:
     def equal(example, expected, message=None, clean=lambda x: x, env={}):
         if not message:
             message = 'Ukaz {0} vrne {1!r} namesto {2!r}.'
-        answer = eval(example, globals(), locals().update(env))
+        local = locals()
+        local.update(env)
+        answer = eval(example, globals(), local)
         if clean(answer) != clean(expected):
             Check.error(message.format(example, answer, expected))
 
