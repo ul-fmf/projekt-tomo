2b7cbefb4a3c286b351ebc645572d65f44e63f01
Matija Pretnar <matija@pretnar.info>
2011-10-28 12:13:54 +0200

    Production now also runs in maintenance mode.

--- fabfile.py
+++ fabfile.py
@@ -57,7 +57,18 @@ def reset_staging_database():
     sudo('''su -c "psql --dbname=tomo --command='CREATE DATABASE tomodev WITH TEMPLATE tomo;'" postgres''')
 
 def restart(destination):
-    sudo('apache2ctl graceful')
+    wsgi = 'tomo' if destination == production else 'tomo-dev'
+    sudo('touch {0}/apache/{1}.wsgi'.format(destination, wsgi))
+
+def lock(destination):
+    lock = 'tomo' if destination == production else 'tomo-dev'
+    sudo('touch {0}/apache/{1}.lock'.format(destination, lock))
+    restart(destination)
+
+def unlock(destination):
+    lock = 'tomo' if destination == production else 'tomo-dev'
+    sudo('rm {0}/apache/{1}.lock'.format(destination, lock))
+    restart(destination)
 
 
 def get_dump():
@@ -75,9 +86,9 @@ def save_dump():
     for dump in dumps:
         local('./manage.py dumpdata --indent=2 {0} > fixtures/{0}.json'.format(dump))
 
-def set_apache():
+def reset_apache():
     put('apache/tomo.fmf.uni-lj.si', '/etc/apache2/sites-available/', use_sudo=True)
-    restart("bla")
+    sudo('apache2ctl graceful')
 
 def reset_local():
     local('touch tomo.db')
