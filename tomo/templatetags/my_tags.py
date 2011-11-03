299033893ba007709ea99b964c65c5b1f15c08e1
Matija Pretnar <matija@pretnar.info>
2011-11-03 10:54:42 +0100

    Bundled mathjax markdown extension. Furthermore, we do not need markup app anymore.

--- tomo/templatetags/my_tags.py
+++ tomo/templatetags/my_tags.py
@@ -1,10 +1,29 @@
-from markdown import Markdown
+import markdown
 
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+
+# This code is taken from https://github.com/mayoff/python-markdown-mathjax
+# We put it here instead of in mdx_mathjax.py to ensure it is bundled with app.
+# This is because mdx_mathjax needs to be on PYTHONPATH because of how
+# markdown extensions work in python.
+
+class MathJaxPattern(markdown.inlinepatterns.Pattern):
+
+    def __init__(self):
+        markdown.inlinepatterns.Pattern.__init__(self, r'(?<!\\)(\$\$?)(.+?)\2')
+
+    def handleMatch(self, m):
+        return markdown.AtomicString(m.group(2) + m.group(3) + m.group(2))
+
+class MathJaxExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        # Needs to come before escape matching because \ is pretty important in LaTeX
+        md.inlinePatterns.add('mathjax', MathJaxPattern(), '<escape')
 
 register = template.Library()
-md = Markdown()
+md = markdown.Markdown(extensions=[MathJaxExtension()])
 
 @register.filter
 @stringfilter
@@ -26,3 +45,9 @@ def get(h, key):
 @register.filter
 def get_default(h, key):
     return h.get(key, None)
+
+@register.filter
+@stringfilter
+def tex_markdown(source):
+    return md.convert(source)
+tex_markdown.is_safe = True
