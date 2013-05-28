ee3567f774d596bcd620b5eef7587600b9d80104
Matija Pretnar <matija@pretnar.info>
2013-05-28 08:43:20 +0200

    Reverted to old-style WSGI.

--- project/wsgi/tomodev.py
+++ project/wsgi/tomodev.py
@@ -20,8 +20,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.tomodev")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
