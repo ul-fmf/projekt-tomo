cbd73de74b6f71eb8274d489402b02f13178d6f6
Matija Pretnar <matija@pretnar.info>
2013-06-07 11:40:33 +0200

    Corrected maintenance mode.

--- libs/wsgi.py
+++ libs/wsgi.py
@@ -2,11 +2,11 @@ import os
 
 def maintenance(application, lock, message):
     if os.path.exists(lock):
-        return application
-    else:
         def maintenance_wsgi_application(environ, start_response):
             start_response('503 Service Unavailable',
                            [('Content-Type', 'text/html; charset=UTF-8')])
             with open(message) as f:
                 return [f.read()]
         return maintenance_wsgi_application
+    else:
+        return application
