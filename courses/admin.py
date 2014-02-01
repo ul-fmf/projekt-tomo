c62475696b6176e58502b394706c21f68f446bb8
Matija Pretnar <matija@pretnar.info>
2014-02-01 05:41:48 +0100

    Moved courses models to courses.admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from courses.models import ProblemSet, Course
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(ProblemSet)
--- tomo/admin.py
+++ tomo/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from courses.models import ProblemSet, Course
 from tomo.models import Language, Problem, Part, Submission, Attempt
 
 class PartInline(admin.StackedInline):
@@ -47,39 +46,11 @@ class SubmissionAdmin(admin.ModelAdmin):
     save_on_top = True
     search_fields = ['user__username', 'problem__title']
 
-class ProblemSetInline(admin.StackedInline):
-    model = ProblemSet
-    extra = 0
-
-class ProblemInline(admin.StackedInline):
-    model = Problem
-    extra = 0
-
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [ProblemSetInline]
-    list_display = ('shortname', 'name', 'timestamp')
-    list_editable = ('name', )
-    save_on_top = True
-
 class PartAdmin(admin.ModelAdmin):
     list_display = ('id', 'problem', 'timestamp',)
 
-class ProblemSetAdmin(admin.ModelAdmin):
-    fieldsets = (
-        (None, {
-            'fields': ('course', 'title', 'description', ('visible', 'solution_visibility'))
-        }),
-    )
-    inlines = [ProblemInline]
-    list_display = ('title', 'visible', 'solution_visibility', 'timestamp')
-    list_filter = ('visible', 'solution_visibility')
-    list_editable = ('visible', 'solution_visibility')
-    save_on_top = True
-    search_fields = ('problems__title', 'title', 'description')
 
 
-admin.site.register(Course, CourseAdmin)
-admin.site.register(ProblemSet, ProblemSetAdmin)
 admin.site.register(Language)
 admin.site.register(Problem, ProblemAdmin)
 admin.site.register(Submission, SubmissionAdmin)
