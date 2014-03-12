cabed32ec08a88f4881140aa7dc172fda4f12345
Matija Pretnar <matija@pretnar.info>
2014-03-12 21:31:31 +0100

    Small fixes.

--- courses/views.py
+++ courses/views.py
@@ -3,22 +3,22 @@ from tomo.utils import verify
 from .models import ProblemSet
 
 
-def problemset_move(request, problemset_id, shift):
-    problemset = get_object_or_404(ProblemSet, id=problemset_id)
+def problemset_move(request, pk, shift):
+    problemset = get_object_or_404(ProblemSet, id=pk)
     verify(problemset.course.has_teacher(request.user))
     problemset.move(shift)
     return redirect(request.META.get('HTTP_REFERER', problemset))
 
 
-def problemset_toggle_visible(request, problemset_id):
-    problemset = get_object_or_404(ProblemSet, id=problemset_id)
+def problemset_toggle_visible(request, pk):
+    problemset = get_object_or_404(ProblemSet, id=pk)
     verify(problemset.course.has_teacher(request.user))
     problemset.toggle_visible()
     return redirect(request.META.get('HTTP_REFERER', problemset))
 
 
-def problemset_toggle_solution_visibility(request, problemset_id):
-    problemset = get_object_or_404(ProblemSet, id=problemset_id)
+def problemset_toggle_solution_visibility(request, pk):
+    problemset = get_object_or_404(ProblemSet, id=pk)
     verify(problemset.course.has_teacher(request.user))
     problemset.toggle_solution_visibility()
     return redirect(request.META.get('HTTP_REFERER', problemset))
