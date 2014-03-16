5c796b87cd96ceb92bd79f1a3511c6d11b02c585
Matija Pretnar <matija@pretnar.info>
2014-03-16 23:12:06 +0100

    WSGI loads from virtualenv

--- project/wsgi/tomoprod.py
+++ project/wsgi/tomoprod.py
@@ -12,10 +12,24 @@ http://blog.dscpl.com.au/2012/10/requests-running-in-wrong-django.html
 import os
 os.environ["DJANGO_SETTINGS_MODULE"] = "project.settings.tomoprod"
 
-import site
+import sys, site
 base_path = "/srv/tomoprod"
-site.addsitedir(base_path)
-site.addsitedir(os.path.join(base_path, "virtualenv/lib/python2.6/site-packages"))
+ALLDIRS = [base_path, os.path.join(base_path, "virtualenv/lib/python2.6/site-packages")]
+
+# Remember original sys.path.
+prev_sys_path = list(sys.path) 
+
+# Add each new site-packages directory.
+for directory in ALLDIRS:
+  site.addsitedir(directory)
+
+# Reorder sys.path so new directories at the front.
+new_sys_path = [] 
+for item in list(sys.path): 
+    if item not in prev_sys_path: 
+        new_sys_path.append(item) 
+        sys.path.remove(item) 
+sys.path[:0] = new_sys_path 
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
