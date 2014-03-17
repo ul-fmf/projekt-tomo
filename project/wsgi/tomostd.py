9135bdd222704a32cbd6db3a94ea2e8b0ab3b669
Matija Pretnar <matija@pretnar.info>
2014-03-17 07:46:21 +0100

    Switched to default wsgi (path is set in apache)

--- project/wsgi/tomostd.py
+++ project/wsgi/tomostd.py
@@ -4,32 +4,11 @@ WSGI config for projekt-tomo.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-and
-http://blog.dscpl.com.au/2012/10/requests-running-in-wrong-django.html
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/.
 """
 
 import os
-os.environ["DJANGO_SETTINGS_MODULE"] = "project.settings.tomostd"
-
-import sys, site
-base_path = "/srv/tomostd"
-ALLDIRS = [base_path, os.path.join(base_path, "virtualenv/lib/python2.6/site-packages")]
-
-# Remember original sys.path.
-prev_sys_path = list(sys.path) 
-
-# Add each new site-packages directory.
-for directory in ALLDIRS:
-  site.addsitedir(directory)
-
-# Reorder sys.path so new directories at the front.
-new_sys_path = [] 
-for item in list(sys.path): 
-    if item not in prev_sys_path: 
-        new_sys_path.append(item) 
-        sys.path.remove(item) 
-sys.path[:0] = new_sys_path 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.tomostd")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
