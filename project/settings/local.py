053fc7bc4414e5a47468c10d8cdb34f97ac0f141
Matija Pretnar <matija@pretnar.info>
2014-04-23 14:14:12 +0200

    Moved local database to Postgres.

--- project/settings/local.py
+++ project/settings/local.py
@@ -42,8 +42,12 @@ WSGI_APPLICATION = 'project.wsgi.local.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tomo',
+        'USER': 'tomo',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
