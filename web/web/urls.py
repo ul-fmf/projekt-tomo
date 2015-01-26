ac79eea03436b15d8f153decd2979112138ece9f
Gregor Jerše <gregor.jerse@predoslje.com>
2015-01-26 11:13:42 +0100

    Support for Django REST framework

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from attempts.rest import AttemptViewSet
+
+
+router = DefaultRouter()
+router.register(r'attempts', AttemptViewSet)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'web.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
 )
