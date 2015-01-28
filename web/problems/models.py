aedc9f6e5a4998bed75e21e777ab8fa676090ea7
Matija Pretnar <matija@pretnar.info>
2015-01-28 13:15:16 +0100

    Move is_json_string_list and shorten to utils

--- web/problems/models.py
+++ web/problems/models.py
@@ -1,25 +1,6 @@
 import json
-from django.core.exceptions import ValidationError
 from django.db import models
-
-
-def shorten(s, max_length=50):
-    if len(s) < max_length:
-        return s
-    else:
-        return u'{0}...'.format(s[:50])
-
-
-def is_json_string_list(s):
-    try:
-        val = json.loads(s)
-    except ValueError:
-        raise ValidationError('Not a JSON value.')
-    if type(val) is not list:
-        raise ValidationError('Not a JSON list.')
-    for x in val:
-        if type(x) is not unicode:
-            raise ValidationError('Not a JSON list of strings.')
+from utils import is_json_string_list, shorten
 
 
 class Problem(models.Model):
