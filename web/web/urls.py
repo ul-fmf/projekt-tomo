3e0ff8d960f7e3d37d2af046f347d9f0b1d355a3
Gregor Jerše <gregor.jerse@predoslje.com>
2015-01-29 13:17:53 +0100

    Enhance REST support
    Add Token auhtentication to Django REST framework.
    Add REST representation for Problem.

--- web/web/urls.py
+++ web/web/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from attempts.rest import AttemptViewSet
+from problems.rest import ProblemViewSet
 
 
 router = DefaultRouter()
-router.register(r'attempts', AttemptViewSet)
+router.register(r'attempt', AttemptViewSet)
+router.register(r'problems', ProblemViewSet)
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
