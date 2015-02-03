0f3dcf138e1c38b01aa2fc785cba38103f74296a
Gregor Jerše <gregor.jerse@predoslje.com>
2015-02-03 11:16:35 +0100

    REST representation of Problem class
    Add REST representation of Problem model instance. It includes nested
    representation of Part object instances (read-only for now).
    
    Also includes some small fixes to make project pep8 compatible.

--- web/attempts/rest.py
+++ web/attempts/rest.py
@@ -11,17 +11,17 @@ from .models import Attempt
 class WritableJSONField(Field):
     def to_internal_value(self, data):
         return data
-        
+
 
 class JSONStringField(Field):
     """
     Store a JSON object in a TextField.
     When object is received store its json dump.
-    When object is retrieved load JSON object from string representation. 
+    When object is retrieved load JSON object from string representation.
     """
     def to_internal_value(self, data):
         return json.dumps(data)
-    
+
     def to_representation(self, value):
         return json.loads(value)
 
