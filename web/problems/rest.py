1df748dca97f1feaadb3c99525f3f6a33e529050
Gregor Jerše <gregor.jerse@predoslje.com>
2015-02-03 11:16:35 +0100

    REST representation of Problem class
    Add REST representation of Problem model instance. It includes nested
    representation of Part object instances (read-only for now).
    
    Also includes some small fixes to make project pep8 compatible.

--- web/problems/rest.py
+++ web/problems/rest.py
@@ -1,12 +1,40 @@
+import json
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
-from .models import Problem
+from rest_framework.fields import Field
+from .models import Problem, Part
+
+
+class JSONStringField(Field):
+    """
+    Store a JSON object in a TextField.
+    When object is received store its json dump.
+    When object is retrieved load JSON object from string representation.
+    """
+    def to_internal_value(self, data):
+        return json.dumps(data)
+
+    def to_representation(self, value):
+        return json.loads(value)
+
+
+class PartSerializer(ModelSerializer):
+    """
+    Serialize a Part object instance.
+    """
+    secret = JSONStringField()
+
+    class Meta:
+        model = Part
+        exclude = ('_order',)
 
 
 class ProblemSerializer(ModelSerializer):
     """
     Serialize a Problem object.
     """
+    parts = PartSerializer(many=True)
+
     class Meta:
         model = Problem
 
