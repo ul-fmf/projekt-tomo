539467129e2b6b97d86010aeb54b07ff4494e951
Matija Pretnar <matija@pretnar.info>
2015-02-11 09:53:04 +0100

    Minor cleanup

--- web/problems/views.py
+++ web/problems/views.py
@@ -1,35 +1,26 @@
-from django.shortcuts import render
-from django.template import RequestContext, loader
-from django.http import HttpResponse
-from django.core.files import File
-import zipfile
-import StringIO
-
-from problems.models import Problem, Part
+from django.shortcuts import get_object_or_404, render
+from problems.models import Problem
 from utils.views import plain_text
 
 
 def problem_list(request):
-    """Show basic template (index.html) """ 
-    problem_list = Problem.objects.all()
-    context = {'problem_list':problem_list}
-    return render(request, 'problems/problem_list.html', context)
-# Create your views here.
+    """Show a list of all problems."""
+    return render(request, 'problems/problem_list.html', {
+        'problems': Problem.objects.all()
+    })
 
 
-#TODO: create a file from problem data and serve that file to user
-# 1.step: get data: problem title, problem description, part description, part solution, part validation, part secret
-# 2.step: create  a new file and write all the data in it
-# 3.step: make file downloadable
-def problem_detail(request, problem_id):
-    """ Show assignment details like description and parts"""
-    problem = Problem.objects.get(pk=problem_id)
-    context = {'problem': problem}
-    return render(request, 'problems/problem_detail.html', context)
+def problem_detail(request, problem_pk):
+    """Show problem details such as description and parts."""
+    problem = get_object_or_404(Problem, pk=problem_pk)
+    return render(request, 'problems/problem_detail.html', {
+        'problem': problem
+    })
 
-def return_problem_file(request, problem_id):
-    problem = Problem.objects.get(pk=problem_id)
+
+def problem_attempt_file(request, problem_pk):
+    """Download an attempt file for a given problem."""
+    problem = get_object_or_404(Problem, pk=problem_pk)
     user = request.user if request.user.is_authenticated() else None
-    filename, contents = problem.attempt_file(user)
+    filename, contents = problem.attempt_file(user=user)
     return plain_text(filename, contents)
-
