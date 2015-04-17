e2de3aa07d6461178a92d9c0ce70700308a490a6
Matija Pretnar <matija@pretnar.info>
2015-04-17 13:17:32 +0200

    Methods for moving up and down the order

---
+++ web/utils/models.py
@@ -0,0 +1,13 @@
+class OrderWithRespectToMixin(object):
+    def move(self, shift):
+        parent_name = self._meta.order_with_respect_to.name
+        class_name = type(self).__name__
+        parent = getattr(self, parent_name)
+        get_order = getattr(parent, 'get_%s_order' % class_name.lower())
+        set_order = getattr(parent, 'set_%s_order' % class_name.lower())
+        order = get_order()
+        old = order.index(self.id)
+        new = max(0, min(old + int(shift), len(order) - 1))
+        order.insert(new, order.pop(old))
+        set_order(order)
+        parent.save()
