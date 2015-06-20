10513d842ad37c88817ccf89eb171fd6f1187705
Gregor Jerše <gregor.jerse@predoslje.com>
2015-06-20 10:07:39 +0200

    Make file more readable
    And PEP8 compliant.

--- web/problems/urls.py
+++ web/problems/urls.py
@@ -4,11 +4,24 @@ from views import ProblemUpdate
 from views import ProblemCreate
 
 
-urlpatterns = patterns('',
-    url(r'^(?P<problem_pk>\d+)/solutions/$', views.problem_solution, name='problem_solution'),
-    url(r'^(?P<problem_pk>\d+)/download/$', views.problem_attempt_file, name='problem_attempt_file'),
-    url(r'^(?P<problem_pk>\d+)/edit/$', views.problem_edit_file, name='problem_edit_file'),
-    url(r'^(?P<problem_pk>\d+)/move/(?P<shift>-?\d+)/$', views.problem_move, name='problem_move'),
-    url(r'^(?P<pk>\d+)/update/$', ProblemUpdate.as_view(), name='problem_update'),
-    url(r'^create/(?P<problem_set_id>\d+)/$', ProblemCreate.as_view(), name='problem_create')
+urlpatterns = patterns(
+    '',
+    url(r'^(?P<problem_pk>\d+)/solutions/$',
+        views.problem_solution,
+        name='problem_solution'),
+    url(r'^(?P<problem_pk>\d+)/download/$',
+        views.problem_attempt_file,
+        name='problem_attempt_file'),
+    url(r'^(?P<problem_pk>\d+)/edit/$',
+        views.problem_edit_file,
+        name='problem_edit_file'),
+    url(r'^(?P<problem_pk>\d+)/move/(?P<shift>-?\d+)/$',
+        views.problem_move,
+        name='problem_move'),
+    url(r'^(?P<pk>\d+)/update/$',
+        ProblemUpdate.as_view(),
+        name='problem_update'),
+    url(r'^create/(?P<problem_set_id>\d+)/$',
+        ProblemCreate.as_view(),
+        name='problem_create')
 )
