81c8fa167c7075a4568de09a58073823d28c1273
Gregor Jerše <gregor.jerse@predoslje.com>
2015-06-20 11:34:19 +0200

    Tests for basic views

--- web/web/tests.py
+++ web/web/tests.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 from users.models import User
@@ -7,39 +8,55 @@ from courses.models import ProblemSet, Course
 
 # Create your tests here.
 class BasicViewsTestCase(TestCase):
-    fixtures = ['users.json']
+    fixtures = []
 
     def setUp(self):
-        self.user = User.objects.get(username='gregor')
+        self.user = User.objects.create_user(
+            username='gregor',
+            email='gregor@jerse.info',
+            password='gregor')
+
         self.course = Course.objects.create()
         prob_set = ProblemSet.objects.create(course=self.course)
         self.problem = Problem.objects.create(problem_set=prob_set)
-
-        self.public_views = [('login', dict()), ('logout', dict())]
-        self.authenticated_private_views = [
-            ('homepage', dict()),
-            ]
-        self.student_private_views = [
-            ('problem_set_detail', {'problem_set_pk': 1}),
-            ('problem_set_attempts', {'problem_set_pk': 1}),
-            ('course_detail', {'course_pk': 1}),
-            ('problem_solution', {'problem_pk': 1}),
-            ('problem_attempt_file', {'problem_pk': 1}),
-            ]
-        self.teacher_private_views = [
-            ('problem_edit_file', {'problem_pk': 1}),
-            ]
-        self.teacher_redirect_views = [
-            ('problem_set_move', {'problem_set_pk': 1, 'shift': 1}),
-            ('problem_move', {'problem_pk': 1, 'shift': 1}),
-            ('problem_set_move', {'problem_set_pk': 1, 'shift': 1}),
-            ('problem_move', {'problem_pk': 1, 'shift': 1}),
-        ]
+        self.views = {
+            'public':
+                [
+                    ('login', dict()),
+                    ('logout', dict()),
+                ],
+            'authenticated':
+                [
+                    ('homepage', dict())
+                ],
+            'student':
+                [
+                    ('problem_set_detail', {'problem_set_pk': 1}),
+                    ('problem_set_attempts', {'problem_set_pk': 1}),
+                    ('course_detail', {'course_pk': 1}),
+                    ('problem_solution', {'problem_pk': 1}),
+                    ('problem_attempt_file', {'problem_pk': 1}),
+                ],
+            'teacher':
+                [
+                    ('problem_edit_file', {'problem_pk': 1}),
+                ],
+            'teacher_redirect':
+                [
+                    ('problem_set_move', {'problem_set_pk': 1, 'shift': 1}),
+                    ('problem_move', {'problem_pk': 1, 'shift': 1}),
+                    ('problem_set_move', {'problem_set_pk': 1, 'shift': 1}),
+                    ('problem_move', {'problem_pk': 1, 'shift': 1}),
+                ],
+        }
         self.default_redirect_view_name = 'login'
-        self.redirect_view_name = {
-            'problem_move': 'admin:login',
-            'problem_set_move': 'admin:login'}
         self.client = Client()
+
+    def login(self):
+        self.assertTrue(self.client.login(username='gregor', password='gregor'), "Login failed")
+
+    def logout(self):
+        self.client.logout()
 
     def assertCode(self, view, response, code):
         message = "{2}: expected {1}, got {0}.".format(response.status_code, code, view)
@@ -47,7 +64,7 @@ class BasicViewsTestCase(TestCase):
             message = message[:-1] + ' (redirects to {0}).'.format(response.url)
         self.assertEqual(response.status_code, code, message)
 
-    def assertRedirect(self, client, view, kwargs, redirected_view=None):
+    def assertRedirect(self, view, kwargs, redirected_view=None):
         url = reverse(view, kwargs=kwargs)
         response = self.client.get(url)
         self.assertCode(view, response, 302)
@@ -56,61 +73,81 @@ class BasicViewsTestCase(TestCase):
             self.assertRedirects(response, '{0}?next={1}'.format(reverse(redirect_view_name), url))
         return response
 
-    def assertOK(self, client, view, kwargs):
+    def assertOK(self, view, kwargs):
             response = self.client.get(reverse(view, kwargs=kwargs))
             self.assertCode(view, response, 200)
 
-    def assertDenied(self, client, view, kwargs):
+    def assertDenied(self, view, kwargs):
             response = self.client.get(reverse(view, kwargs=kwargs))
             self.assertCode(view, response, 403)
 
-    def testLoginRedirect(self):
-        response = self.client.get('/')
-        self.assertRedirects(response, '{0}{1}'.format(reverse('login'), '?next=/'))
-
-    def testPublicUnauthenticated(self):
-        for view, args in self.public_views:
-            self.assertOK(self.client, view, args)
-
-    def testPrivateUnauthenticated(self):
-        for view, args in self.teacher_private_views + self.student_private_views:
-            redirect_view_name = self.redirect_view_name.get(view, self.default_redirect_view_name)
-            self.assertRedirect(self.client, view, args, redirect_view_name)
+    def testUnauthenticated(self):
+        """
+        Unauthenticated user must be redirected to login page
+        except for public pages.
+        """
+        public_views = [view for view, args in self.views['public']]
+        for view, args in chain.from_iterable(self.views.values()):
+            if view not in public_views:
+                self.assertRedirect(view, args, 'login')
+            else:
+                self.assertOK(view, args)
 
     def testPublicAuthenticated(self):
-        self.client.login(username='gregor', password='gregor')
-        for view, args in self.public_views:
-            self.assertOK(self.client, view, args)
-        self.client.logout()
+        """
+        Authenticated user should receive status 200 on public pages.
+        """
+        try:
+            self.login()
+            for view, args in self.views['public'] + self.views['authenticated']:
+                self.assertOK(view, args)
+        finally:
+            self.logout()
 
     def testPrivateAuthenticated(self):
-        self.client.login(username='gregor', password='gregor')
-        for view, args in self.authenticated_private_views:
-            self.assertOK(self.client, view, args)
-        for view, args in self.student_private_views:
-            self.assertRedirect(self.client, view, args, 'login')
-        for view, args in self.teacher_private_views:
-            self.assertRedirect(self.client, view, args, 'login')
+        """
+        Authenticated user should receive 403 (denied) on non-public pages.
+        """
+        try:
+            self.login()
+            public_views = [view for view, _ in self.views['public'] + self.views['authenticated']]
+            for view, args in chain.from_iterable(self.views.values()):
+                if view not in public_views:
+                    self.assertDenied(view, args)
+        finally:
+            self.logout()
 
-    def testStudentAuthenticated(self):
-        self.course.students.add(self.user)
-        self.client.login(username='gregor', password='gregor')
-        for view, args in self.authenticated_private_views + self.student_private_views:
-            self.assertOK(self.client, view, args)
-        for view, args in self.teacher_private_views + self.teacher_redirect_views:
-            self.assertDenied(self.client, view, args)
-        self.client.logout()
-        self.course.students.remove(self.user)
+    def testStudent(self):
+        """
+        Student should receive 200 on views allowed to students.
+        Students should receive 403 otherwise.
+        """
+        try:
+            self.login()
+            denied = self.views['teacher'] + self.views['teacher_redirect']
+            for view, args in denied + self.views['student']:
+                self.assertDenied(view, args)
+            self.course.students.add(self.user)
+            for view, args in self.views['student']:
+                self.assertOK(view, args)
+            for view, args in denied:
+                self.assertDenied(view, args)
+        finally:
+            self.logout()
+            self.course.students.remove(self.user)
 
-    def testTeacherAuthenticated(self):
-        self.course.teachers.add(self.user)
-        self.client.login(username='gregor', password='gregor')
-        views = (self.authenticated_private_views +
-                 self.student_private_views +
-                 self.teacher_private_views)
-        for view, args in views:
-            self.assertOK(self.client, view, args)
-        for view, args in self.teacher_redirect_views:
-            self.assertRedirect(self.client, view, args)
-        self.client.logout()
-        self.course.students.remove(self.user)
+    def testTeacher(self):
+        """
+        Teacher should always receive 200 or 303 (redirect).
+        """
+        try:
+            self.login()
+            self.course.teachers.add(self.user)
+            redirect_views = [view for view, args in self.views['teacher_redirect']]
+            for view, args in chain.from_iterable(self.views.values()):
+                if view not in redirect_views:
+                    self.assertOK(view, args)
+                else:
+                    self.assertRedirect(view, args)
+        finally:
+            self.logout()
