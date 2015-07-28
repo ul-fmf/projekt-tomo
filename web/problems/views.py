b9a7e7eebf5d037026af05e4857d1ba94edf364d
Sonja Jerše <sonja.jerse@gmail.com>
2015-07-28 21:02:23 +0200

    add copy problem form

--- web/problems/views.py
+++ web/problems/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.admin.views.decorators import staff_member_required
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.forms import Form, IntegerField
+from django.http import HttpResponse
 from rest_framework.reverse import reverse
 from problems.models import Problem, Part
-from attempts.models import Attempt
 from courses.models import ProblemSet
 from utils.views import plain_text
 from utils import verify
@@ -72,7 +72,7 @@ class ProblemUpdate(UpdateView):
 
 class ProblemDelete(DeleteView):
     '''
-        Delete a problem and all it's parts and attempts.
+    Delete a problem and all it's parts and attempts.
     '''
     model = Problem
 
@@ -86,56 +86,71 @@ class ProblemDelete(DeleteView):
 
     def get_context_data(self, **kwargs):
         context = super(ProblemDelete, self).get_context_data(**kwargs)
-        #problem_set = get_object_or_404(ProblemSet, id=self.kwargs['problem_set_id'])
-        #context['problem_set'] = problem_set
-#         attempts = {}
-#         submissions = {}
-#         user_ids = set()
-
-#         active_attempts = Attempt.objects.active().for_problem(self.object)
-#         for attempt in active_attempts.select_related('submission__user'):
-#             user_id = attempt.submission.user_id
-#             user_ids.add(user_id)
-#             user_attempts = attempts.get(user_id, {})
-#             user_attempts[attempt.part_id] = attempt
-#             attempts[user_id] = user_attempts
-#             submissions[user_id] = attempt.submission
-#         parts = self.object.parts.all()
-#         sorted_attempts = []
-#         for user in User.objects.filter(id__in=user_ids).order_by('last_name'):
-#             sorted_attempts.append((user, submissions[user.id], [attempts[user.id].get(part.id) for part in parts]))
-#         context['attempts'] = sorted_attempts
         return context
+
+
+class CopyProblemForm(Form):
+    problem_set_id = IntegerField(label='Problem set id')
 
 
 #teacher status required
 #TODO: problem_copy - to copy a problem
-def problem_copy(request, problem_pk, problem_set_pk):
-    original_problem = Problem.objects.get(pk=problem_pk)
-    verify(request.user.can_edit_problem_set(original_problem.problem_set))
+def copy_form(request, problem_pk):
+    """
+    Show and react to CopyForm.
+    """
+    def problem_copy(user, original_problem, new_problem_set):
+        """
+        Copies the given problem to the given problem set.
+        Verifies if:
+        a) user can edit the problem.
+        b) user can edit the problem set.
+        Returns the copy of the given problem.
+        """
+        verify(user.can_edit_problem_set(original_problem.problem_set))
+        verify(request.user.can_edit_problem_set(new_problem_set))
+        new_problem = Problem()
+        new_problem.title = original_problem.title
+        new_problem.description = original_problem.description
+        new_problem.problem_set = new_problem_set
+        new_problem.tags = original_problem.tags
+        new_problem.save()
+        original_parts = original_problem.parts.all()
+        for original_part in original_parts:
+            new_part = Part()
+            new_part.problem = new_problem
+            new_part.description = original_part.description
+            new_part.solution = original_part.solution
+            new_part.validation = original_part.validation
+            new_part.secret = original_part.secret
+            new_part.save()
+        return new_problem
 
-    new_problem_set = ProblemSet.objects.get(pk=problem_set_pk)
-    verify(request.user.can_edit_problem_set(new_problem_set))
+    problem = Problem.objects.get(pk=problem_pk)
+    if request.method == 'POST':
+        form = CopyProblemForm(request.POST)
+        if form.is_valid():
+            # perform the actual copy process
+            problem_set_pk = form.cleaned_data['problem_set_id']
+            problem_set = ProblemSet.objects.get(pk=problem_set_pk)
+            problem_copy(request.user, problem, problem_set)
+            return redirect(problem_set)
+        else:
+            #pass  # TODO: handle errors
+            response = HttpResponse("Please select a problem set.")
+            return response
 
-    new_problem = Problem()
-    new_problem.title = original_problem.title
-    new_problem.description = original_problem.description
-    new_problem.problem_set = new_problem_set
-    new_problem.tags = original_problem.tags
-    new_problem.save()
-
-    original_parts = original_problem.parts.all()
-
-    for original_part in original_parts:
-        new_part = Part()
-        new_part.problem = new_problem
-        new_part.description = original_part.description
-        new_part.solution = original_part.solution
-        new_part.validation = original_part.validation
-        new_part.secret = original_part.secret
-        new_part.save()
-
-    return redirect(new_problem.problem_set)
+    else:
+        form = CopyProblemForm()
+        courses = request.user.taught_courses.all()
+        return render(
+            request, 'courses/problem_copy_form.html',
+            {
+                'form': form,
+                'courses': courses,
+                'problem': problem,
+            }
+        )
 
 
 def get_courses_and_problem_sets(request):
