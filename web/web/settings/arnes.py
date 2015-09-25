d109a98b1110719e5b566a73d3f2d27bc7ab41e3
Matija Pretnar <matija@pretnar.info>
2015-09-25 09:55:48 +0200

    Fix shibboleth settings

--- web/web/settings/arnes.py
+++ web/web/settings/arnes.py
@@ -41,6 +41,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'shibboleth.middleware.ShibbolethRemoteUserMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
