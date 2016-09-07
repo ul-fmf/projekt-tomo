93e6a34225561f7a7c3e981b1b99c3645cb2ff46
Gregor Jerše <gregor.jerse@predoslje.com>
2016-09-07 14:01:13 +0200

    Add LocaleMiddleware to arnes settings

--- web/web/settings/arnes.py
+++ web/web/settings/arnes.py
@@ -24,6 +24,7 @@ STATIC_URL = '/static/'
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
