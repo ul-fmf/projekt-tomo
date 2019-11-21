8a470e8a91cd2c7571051373a9d468afe6cb1869
KraljSamo <samokralj95@gmail.com>
2019-11-21 10:42:26 +0100

    changed statistics name, user -> student

--- web/tomo_statistics/views.py
+++ web/tomo_statistics/views.py
@@ -35,16 +35,16 @@ def course_submission_history_problemset(request, course_pk, problemset_pk):
 @login_required
 def course_user_submission_history_problemset(request, course_pk, problemset_pk, user_pk):
     course = get_object_or_404(Course, pk=course_pk)
-    user = get_object_or_404(User, pk=user_pk)
+    student = get_object_or_404(User, pk=user_pk)
     problemset = get_object_or_404(ProblemSet, pk=problemset_pk)
     verify(request.user.can_view_course_statistics(course))
-    user_history = get_submission_history(problemset, user)
+    user_history = get_submission_history(problemset, student)
     return render(request,
                  "statistics/user_submission_history.html",
                 {
                     'course' : course,
                     'problemset' : problemset,
-                    'user' : user,
+                    'student' : student,
                     'history' : user_history
                 })
 
@@ -53,7 +53,7 @@ def user_problem_solution_at_time(request, historical_attempt_pk):
     historical_attempt = get_object_or_404(HistoricalAttempt, pk=historical_attempt_pk)
     problem = historical_attempt.part.problem
     course = problem.problem_set.course
-    user = historical_attempt.user
+    student = historical_attempt.user
     problem_state = get_problem_solve_state_at_time(historical_attempt)
     verify(request.user.can_view_course_statistics(course))
     return render(request,
@@ -61,7 +61,7 @@ def user_problem_solution_at_time(request, historical_attempt_pk):
                  {
                     "historical_attempt" : historical_attempt,
                     "problem" : problem,
-                    "user" : user,
+                    "student" : student,
                     "parts" : problem_state,
                  })
 
