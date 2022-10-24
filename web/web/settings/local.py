22bf7b62173480d5ea6aec9a877e585ce011aaf2
Matija Pretnar <matija@pretnar.info>
2022-10-24 13:56:21 +0200

    Izklopil Nose, ki ni več podprt

--- web/web/settings/local.py
+++ web/web/settings/local.py
@@ -29,11 +29,3 @@ LOGIN_URL = "/accounts/login/"
 LOGOUT_URL = "/accounts/logout/"
 LOGIN_REDIRECT_URL = "/"
 SUBMISSION_URL = "http://127.0.0.1:8000"
-
-# Use nose to run all tests
-TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
-# Tell nose to measure coverage on the 'problems', 'attemtps', 'courses' and 'users' apps
-NOSE_ARGS = [
-    "--with-coverage",
-    "--cover-package=problems,attempts,courses,users,utils",
-]
