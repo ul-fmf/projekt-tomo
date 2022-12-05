a043618f4ce06f2680b31918bf1d33b58c49009d
Matija Pretnar <matija@pretnar.info>
2022-12-05 14:04:54 +0100

    Further consolidate problem_set_urls

--- web/courses/urls.py
+++ web/courses/urls.py
@@ -5,92 +5,121 @@ from .views import ProblemSetCreate, ProblemSetDelete, ProblemSetUpdate
 
 problem_set_urls = [
     path(
-        "",
-        views.problem_set_detail,
-        name="problem_set_detail",
+        "create/",
+        ProblemSetCreate.as_view(),
+        name="problem_set_create",
     ),
     path(
-        "attempt/",
-        views.problem_set_attempt,
-        name="problem_set_attempt",
-    ),
-    path(
-        "static/",
-        views.problem_set_static,
-        name="problem_set_static",
-    ),
-    path(
-        "izpit/",
-        views.problem_set_izpit,
-        name="problem_set_izpit",
-    ),
-    path(
-        "tex/",
-        views.problem_set_tex,
-        name="problem_set_tex",
-    ),
-    path(
-        "edit/",
-        views.problem_set_edit,
-        name="problem_set_edit",
-    ),
-    path(
-        "results/",
-        views.problem_set_results,
-        name="problem_set_results",
-    ),
-    path(
-        "move/",
-        views.problem_set_move,
-        name="problem_set_move",
-    ),
-    path(
-        "toggle_visible/",
-        views.problem_set_toggle_visible,
-        name="problem_set_toggle_visible",
-    ),
-    path(
-        "toggle_solution_visibility/",
-        views.problem_set_toggle_solution_visibility,
-        name="problem_set_toggle_solution_visibility",
-    ),
-    path(
-        "progress/",
-        views.problem_set_progress,
-        name="problem_set_progress",
-    ),
-    path(
-        "progress/groups/<int:group_pk>",
-        views.problem_set_progress_groups,
-        name="problem_set_progress_groups",
-    ),
-    path(
-        "update/",
-        ProblemSetUpdate.as_view(),
-        name="problem_set_update",
-    ),
-    path(
-        "delete/",
-        ProblemSetDelete.as_view(),
-        name="problem_set_delete",
+        "<int:problem_set_pk>/",
+        include(
+            [
+                path(
+                    "",
+                    views.problem_set_detail,
+                    name="problem_set_detail",
+                ),
+                path(
+                    "attempt/",
+                    views.problem_set_attempt,
+                    name="problem_set_attempt",
+                ),
+                path(
+                    "static/",
+                    views.problem_set_static,
+                    name="problem_set_static",
+                ),
+                path(
+                    "izpit/",
+                    views.problem_set_izpit,
+                    name="problem_set_izpit",
+                ),
+                path(
+                    "tex/",
+                    views.problem_set_tex,
+                    name="problem_set_tex",
+                ),
+                path(
+                    "edit/",
+                    views.problem_set_edit,
+                    name="problem_set_edit",
+                ),
+                path(
+                    "results/",
+                    views.problem_set_results,
+                    name="problem_set_results",
+                ),
+                path(
+                    "move/",
+                    views.problem_set_move,
+                    name="problem_set_move",
+                ),
+                path(
+                    "toggle_visible/",
+                    views.problem_set_toggle_visible,
+                    name="problem_set_toggle_visible",
+                ),
+                path(
+                    "toggle_solution_visibility/",
+                    views.problem_set_toggle_solution_visibility,
+                    name="problem_set_toggle_solution_visibility",
+                ),
+                path(
+                    "progress/",
+                    views.problem_set_progress,
+                    name="problem_set_progress",
+                ),
+                path(
+                    "progress/groups/<int:group_pk>",
+                    views.problem_set_progress_groups,
+                    name="problem_set_progress_groups",
+                ),
+                path(
+                    "update/",
+                    ProblemSetUpdate.as_view(),
+                    name="problem_set_update",
+                ),
+                path(
+                    "delete/",
+                    ProblemSetDelete.as_view(),
+                    name="problem_set_delete",
+                ),
+            ]
+        ),
     ),
 ]
 
 groups_urls = [
     path(
-        "update/",
-        views.course_groups_update,
-        name="course_groups_update",
+        "",
+        views.course_groups,
+        name="course_groups",
     ),
     path(
-        "delete/",
-        views.course_groups_delete,
-        name="course_groups_delete",
+        "create/",
+        views.course_groups_create,
+        name="course_groups_create",
     ),
     path(
-        "confirm_delete/",
-        views.course_groups_confirm_delete,
-        name="course_groups_confirm_delete",
+        "<int:group_pk>/",
+        include(
+            [
+                path(
+                    "update/",
+                    views.course_groups_update,
+                    name="course_groups_update",
+                ),
+                path(
+                    "delete/",
+                    views.course_groups_delete,
+                    name="course_groups_delete",
+                ),
+                path(
+                    "confirm_delete/",
+                    views.course_groups_confirm_delete,
+                    name="course_groups_confirm_delete",
+                ),
+            ]
+        ),
     ),
 ]
 
@@ -126,23 +155,8 @@ course_urls = [
         views.course_progress,
         name="course_progress",
     ),
-    path(
-        "problem_set/create/",
-        ProblemSetCreate.as_view(),
-        name="problem_set_create",
-    ),
-    path("problem_set/<int:problem_set_pk>/", include(problem_set_urls)),
-    path(
-        "groups/",
-        views.course_groups,
-        name="course_groups",
-    ),
-    path(
-        "groups/create/",
-        views.course_groups_create,
-        name="course_groups_create",
-    ),
-    path("groups/<int:group_pk>/", include(groups_urls)),
+    path("groups/", include(groups_urls)),
+    path("problem_set/", include(problem_set_urls)),
 ]
 
 urlpatterns = [
