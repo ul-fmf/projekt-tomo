fc648d66ed2a00a511e4f78dab3d0d46a3fe038a
Filip Koprivec <jO-Osko@users.noreply.github.com>
2023-02-19 04:40:30 +0100

    Linters (#280)
    Closes #253

--- web/problems/templates/python/attempt.py
+++ web/problems/templates/python/attempt.py
@@ -8,6 +8,13 @@
 # =============================================================================
 {{ solution_attempt|safe }}{% endfor %}
 
+# Kode od tu naprej NE SPREMINJAJTE!
+# fmt: off
+
+"Če vam Python sporoča, da je v tej vrstici sintaktična napaka,"
+"se napaka v resnici skriva v zadnjih vrsticah vaše kode."
+
+"Kode od tu naprej NE SPREMINJAJTE!"
 
 
 
@@ -109,60 +116,12 @@
 
 # ============================================================================@
 
-'Če vam Python sporoča, da je v tej vrstici sintaktična napaka,'
-'se napaka v resnici skriva v zadnjih vrsticah vaše kode.'
+"Če vam Python sporoča, da je v tej vrstici sintaktična napaka,"
+"se napaka v resnici skriva v zadnjih vrsticah vaše kode."
 
-'Kode od tu naprej NE SPREMINJAJTE!'
+"Kode od tu naprej NE SPREMINJAJTE!"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# isort: off
 import json
 import os
 import re
@@ -171,35 +130,34 @@ import sys
 import traceback
 import urllib.error
 import urllib.request
-
 {% include 'python/check.py' %}
 
 def _validate_current_file():
     def extract_parts(filename):
-        with open(filename, encoding='utf-8') as f:
+        with open(filename, encoding="utf-8") as f:
             source = f.read()
         part_regex = re.compile(
-            r'# =+@(?P<part>\d+)=\s*\n' # beginning of header
-            r'(\s*#( [^\n]*)?\n)+?'     # description
-            r'\s*# =+\s*?\n'            # end of header
-            r'(?P<solution>.*?)'        # solution
-            r'(?=\n\s*# =+@)',          # beginning of next part
-            flags=re.DOTALL | re.MULTILINE
+            r"# =+@(?P<part>\d+)=\s*\n"  # beginning of header
+            r"(\s*#( [^\n]*)?\n)+?"  # description
+            r"\s*# =+\s*?\n"  # end of header
+            r"(?P<solution>.*?)"  # solution
+            r"(?=\n\s*# =+@)",  # beginning of next part
+            flags=re.DOTALL | re.MULTILINE,
         )
-        parts = [{
-            'part': int(match.group('part')),
-            'solution': match.group('solution')
-        } for match in part_regex.finditer(source)]
+        parts = [
+            {"part": int(match.group("part")), "solution": match.group("solution")}
+            for match in part_regex.finditer(source)
+        ]
         # The last solution extends all the way to the validation code,
         # so we strip any trailing whitespace from it.
-        parts[-1]['solution'] = parts[-1]['solution'].rstrip()
+        parts[-1]["solution"] = parts[-1]["solution"].rstrip()
         return parts
 
     def backup(filename):
         backup_filename = None
         suffix = 1
         while not backup_filename or os.path.exists(backup_filename):
-            backup_filename = '{0}.{1}'.format(filename, suffix)
+            backup_filename = "{0}.{1}".format(filename, suffix)
             suffix += 1
         shutil.copy(filename, backup_filename)
         return backup_filename
@@ -209,85 +167,89 @@ def _validate_current_file():
         for part in parts:
             if Check.has_solution(part):
                 submitted_part = {
-                    'part': part['part'],
-                    'solution': part['solution'],
-                    'valid': part['valid'],
-                    'secret': [x for (x, _) in part['secret']],
-                    'feedback': json.dumps(part['feedback']),
+                    "part": part["part"],
+                    "solution": part["solution"],
+                    "valid": part["valid"],
+                    "secret": [x for (x, _) in part["secret"]],
+                    "feedback": json.dumps(part["feedback"]),
                 }
-                if 'token' in part:
-                    submitted_part['token'] = part['token']
+                if "token" in part:
+                    submitted_part["token"] = part["token"]
                 submitted_parts.append(submitted_part)
-        data = json.dumps(submitted_parts).encode('utf-8')
-        headers = {
-            'Authorization': token,
-            'content-type': 'application/json'
-        }
+        data = json.dumps(submitted_parts).encode("utf-8")
+        headers = {"Authorization": token, "content-type": "application/json"}
         request = urllib.request.Request(url, data=data, headers=headers)
         # This is a workaround because some clients (and not macOS ones!) report
         # <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: certificate has expired (_ssl.c:1129)>
         import ssl
+
         context = ssl._create_unverified_context()
         response = urllib.request.urlopen(request, context=context)
         # When the issue is resolved, the following should be used
         # response = urllib.request.urlopen(request)
-        return json.loads(response.read().decode('utf-8'))
+        return json.loads(response.read().decode("utf-8"))
 
     def update_attempts(old_parts, response):
         updates = {}
-        for part in response['attempts']:
-            part['feedback'] = json.loads(part['feedback'])
-            updates[part['part']] = part
+        for part in response["attempts"]:
+            part["feedback"] = json.loads(part["feedback"])
+            updates[part["part"]] = part
         for part in old_parts:
-            valid_before = part['valid']
-            part.update(updates.get(part['part'], {}))
-            valid_after = part['valid']
+            valid_before = part["valid"]
+            part.update(updates.get(part["part"], {}))
+            valid_after = part["valid"]
             if valid_before and not valid_after:
-                wrong_index = response['wrong_indices'].get(str(part['part']))
+                wrong_index = response["wrong_indices"].get(str(part["part"]))
                 if wrong_index is not None:
-                    hint = part['secret'][wrong_index][1]
+                    hint = part["secret"][wrong_index][1]
                     if hint:
-                        part['feedback'].append('Namig: {}'.format(hint))
-
+                        part["feedback"].append("Namig: {}".format(hint))
 
     filename = os.path.abspath(sys.argv[0])
     file_parts = extract_parts(filename)
     Check.initialize(file_parts)
 {% for part, _, token in parts %}
     if Check.part():
-        Check.current_part['token'] = '{{ token }}'
+        Check.current_part[
+            "token"
+        ] = "{{ token }}"
         try:
             {{ part.validation|default:"pass"|indent:"            "|safe }}
         except:
-            Check.error("Testi sprožijo izjemo\n  {0}",
-                        "\n  ".join(traceback.format_exc().split("\n"))[:-2])
+            Check.error(
+                "Testi sprožijo izjemo\n  {0}",
+                "\n  ".join(traceback.format_exc().split("\n"))[:-2],
+            )
 {% endfor %}
-    print('Shranjujem rešitve na strežnik... ', end="")
+    print("Shranjujem rešitve na strežnik... ", end="")
     try:
-        url = '{{ submission_url }}'
-        token = 'Token {{ authentication_token }}'
+        url = "{{ submission_url }}"
+        token = "Token {{ authentication_token }}"
         response = submit_parts(Check.parts, url, token)
     except urllib.error.URLError:
-        message = ('\n'
-                   '-------------------------------------------------------------------\n'
-                   'PRI SHRANJEVANJU JE PRIŠLO DO NAPAKE!\n'
-                   'Preberite napako in poskusite znova ali se posvetujte z asistentom.\n'
-                   '-------------------------------------------------------------------\n')
+        message = (
+            "\n"
+            "-------------------------------------------------------------------\n"
+            "PRI SHRANJEVANJU JE PRIŠLO DO NAPAKE!\n"
+            "Preberite napako in poskusite znova ali se posvetujte z asistentom.\n"
+            "-------------------------------------------------------------------\n"
+        )
         print(message)
         traceback.print_exc()
         print(message)
         sys.exit(1)
     else:
-        print('Rešitve so shranjene.')
+        print("Rešitve so shranjene.")
         update_attempts(Check.parts, response)
-        if 'update' in response:
-            print('{% trans "Updating file" %}... ', end="")
+        if "update" in response:
+            print("{% trans 'Updating file' %}... ", end="")
             backup_filename = backup(filename)
-            with open(__file__, 'w', encoding='utf-8') as f:
-                f.write(response['update'])
-            print('{% trans "Previous file has been renamed to" %} {0}.'.format(backup_filename))
-            print('{% trans "If the file did not refresh in your editor, close and reopen it." %}')
+            with open(__file__, "w", encoding="utf-8") as f:
+                f.write(response["update"])
+            print("{% trans 'Previous file has been renamed to' %} {0}.".format(backup_filename))
+            print("{% trans 'If the file did not refresh in your editor, close and reopen it.' %}")
     Check.summarize()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     _validate_current_file()
--- web/problems/templates/python/edit.py
+++ web/problems/templates/python/edit.py
@@ -1,8 +1,8 @@
-{% load i18n %}with open(__file__, encoding='utf-8') as f:
+{% load i18n %}with open(__file__, encoding="utf-8") as f:
     source = f.read()
-exec(source[source.find("# =L=I=B=""R=A=R=Y=@="):])
+exec(source[source.find("# =L=I=B=" "R=A=R=Y=@="):])
 problem = extract_problem(__file__)
-Check.initialize(problem['parts'])
+Check.initialize(problem["parts"])
 
 # =============================================================================
 # {{ problem.title|safe }}{% if problem.description %}
@@ -47,13 +47,15 @@ Check.part()
 # ===========================================================================@=
 # {% trans "Do not change this line or anything below it." %}
 # =============================================================================
+# fmt: off
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     _validate_current_file()
 
 # =L=I=B=R=A=R=Y=@=
 
+# isort: off
+import io
 import json
 import os
 import re
@@ -62,106 +64,112 @@ import sys
 import traceback
 import urllib.error
 import urllib.request
-
 {% include 'python/check.py' %}
 
 def extract_problem(filename):
     def strip_hashes(description, add_newline=False):
         if description is None:
-            return ''
+            return ""
         else:
             lines = description.strip().splitlines()
-            return "\n".join(line[line.index('#')+2:] for line in lines) \
-                 + ('\n' if add_newline else '')
+            extracted_lines = "\n".join(line[line.index("#") + 2 :] for line in lines)
+            return extracted_lines + ('\n' if add_newline else '')
 
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, encoding="utf-8") as f:
         source = f.read()
     part_regex = re.compile(
-        r'# ===+@(?P<part>\d+)=\s*\n'             # beginning of part header
-        r'(?P<description>(\s*#( [^\n]*)?\n)+?)'  # description
-        r'(\s*# ---+\s*\n'                        # optional beginning of template
-        r'(?P<template>(\s*#( [^\n]*)?\n)*))?'    # solution template
-        r'\s*# ===+\s*?\n'                        # end of part header
-        r'(?P<solution>.*?)'                      # solution
-        r'^Check\s*\.\s*part\s*\(\s*\)\s*?(?=\n)' # beginning of validation
-        r'(?P<validation>.*?)'                    # validation
-        r'(?=\n\s*(# )?# =+@)',                   # beginning of next part
-        flags=re.DOTALL | re.MULTILINE
+        r"# ===+@(?P<part>\d+)=\s*\n"  # beginning of part header
+        r"(?P<description>(\s*#( [^\n]*)?\n)+?)"  # description
+        r"(\s*# ---+\s*\n"  # optional beginning of template
+        r"(?P<template>(\s*#( [^\n]*)?\n)*))?"  # solution template
+        r"\s*# ===+\s*?\n"  # end of part header
+        r"(?P<solution>.*?)"  # solution
+        r"^Check\s*\.\s*part\s*\(\s*\)\s*?(?=\n)"  # beginning of validation
+        r"(?P<validation>.*?)"  # validation
+        r"(?=\n\s*(# )?# =+@)",  # beginning of next part
+        flags=re.DOTALL | re.MULTILINE,
     )
-    parts = [{
-        'part': int(match.group('part')),
-        'description': strip_hashes(match.group('description')),
-        'solution': match.group('solution').strip(),
-        'template': strip_hashes(match.group('template'), add_newline=True),
-        'validation': match.group('validation').strip(),
-        'problem': {{ problem.id }}
-    } for match in part_regex.finditer(source)]
+    parts = [
+        {
+            "part": int(match.group("part")),
+            "description": strip_hashes(match.group("description")),
+            "solution": match.group("solution").strip(),
+            "template": strip_hashes(match.group("template"), add_newline=True),
+            "validation": match.group("validation").strip(),
+            "problem": {{problem.id}},
+        }
+        for match in part_regex.finditer(source)
+    ]
     problem_match = re.search(
-        r'^\s*# =+\s*\n'                          # beginning of header
-        r'^\s*# (?P<title>[^\n]*)\n'              # title
-        r'(?P<description>(^\s*#( [^\n]*)?\n)*?)' # description
-        r'(?=\s*(# )?# =+@)',                     # beginning of first part
-        source, flags=re.DOTALL | re.MULTILINE)
+        r"^\s*# =+\s*\n"  # beginning of header
+        r"^\s*# (?P<title>[^\n]*)\n"  # title
+        r"(?P<description>(^\s*#( [^\n]*)?\n)*?)"  # description
+        r"(?=\s*(# )?# =+@)",  # beginning of first part
+        source,
+        flags=re.DOTALL | re.MULTILINE,
+    )
     return {
-        'title': problem_match.group('title').strip(),
-        'description': strip_hashes(problem_match.group('description')),
-        'parts': parts,
-        'id': {{ problem.id }},
-        'problem_set': {{ problem.problem_set.id }}
+        "title": problem_match.group("title").strip(),
+        "description": strip_hashes(problem_match.group("description")),
+        "parts": parts,
+        "id": {{ problem.id }},
+        "problem_set": {{ problem.problem_set.id }},
     }
+
 
 def _validate_current_file():
     def backup(filename):
         backup_filename = None
         suffix = 1
         while not backup_filename or os.path.exists(backup_filename):
-            backup_filename = '{0}.{1}'.format(filename, suffix)
+            backup_filename = "{0}.{1}".format(filename, suffix)
             suffix += 1
         shutil.copy(filename, backup_filename)
         return backup_filename
 
     def submit_problem(problem, url, token):
-        for part in problem['parts']:
-            part['secret'] = [x for (x, _) in part['secret']]
-            if part['part']:
-                part['id'] = part['part']
-            del part['part']
-            del part['feedback']
-            del part['valid']
-        data = json.dumps(problem).encode('utf-8')
-        headers = {
-            'Authorization': token,
-            'content-type': 'application/json'
-        }
+        for part in problem["parts"]:
+            part["secret"] = [x for (x, _) in part["secret"]]
+            if part["part"]:
+                part["id"] = part["part"]
+            del part["part"]
+            del part["feedback"]
+            del part["valid"]
+        data = json.dumps(problem).encode("utf-8")
+        headers = {"Authorization": token, "content-type": "application/json"}
         request = urllib.request.Request(url, data=data, headers=headers)
         response = urllib.request.urlopen(request)
-        return json.loads(response.read().decode('utf-8'))
+        return json.loads(response.read().decode("utf-8"))
 
     Check.summarize()
-    if all(part['valid'] for part in problem['parts']):
-        print('{% trans "The problem is correctly formulated." %}')
-        if input('{% trans "Should I save it on the server [yes/NO]" %}') == '{% trans "yes" %}':
-            print('{% trans "Saving problem to the server" %}...', end="")
+    if all(part["valid"] for part in problem["parts"]):
+        print("{% trans 'The problem is correctly formulated.' %}")
+        if input("{% trans 'Should I save it on the server [yes/NO]' %}") == "{% trans 'yes' %}":
+            print("{% trans 'Saving problem to the server' %}...", end="")
             try:
-                url = '{{ submission_url }}'
-                token = 'Token {{ authentication_token }}'
+                url = "{{ submission_url }}"
+                token = "Token {{ authentication_token }}"
                 response = submit_problem(problem, url, token)
-                if 'update' in response:
-                    print('{% trans "Updating file" %}... ', end="")
+                if "update" in response:
+                    print("{% trans 'Updating file' %}... ", end="")
                     backup_filename = backup(__file__)
-                    with open(__file__, 'w', encoding='utf-8') as f:
-                        f.write(response['update'])
-                    print('{% trans "Previous file has been renamed to" %} {0}.'.format(backup_filename))
-                    print('{% trans "If the file did not refresh in your editor, close and reopen it." %}')
+                    with open(__file__, "w", encoding="utf-8") as f:
+                        f.write(response["update"])
+                    print(
+                        "{% trans 'Previous file has been renamed to' %} {0}.".format(backup_filename)
+                    )
+                    print(
+                        "{% trans 'If the file did not refresh in your editor, close and reopen it.' %}"
+                    )
             except urllib.error.URLError as response:
-                message = json.loads(response.read().decode('utf-8'))
-                print('\n{% trans "AN ERROR OCCURED WHEN TRYING TO SAVE THE PROBLEM!" %}')
+                message = json.loads(response.read().decode("utf-8"))
+                print("\n{% trans 'AN ERROR OCCURED WHEN TRYING TO SAVE THE PROBLEM!' %}")
                 if message:
-                    print('  ' + '\n  '.join(message.splitlines()))
-                print('{% trans "Please, try again." %}')
+                    print("  " + "\n  ".join(message.splitlines()))
+                print("{% trans 'Please, try again.' %}")
             else:
-                print('{% trans "Problem saved." %}')
+                print("{% trans 'Problem saved.' %}")
         else:
-            print('{% trans "Problem was not saved." %}')
+            print("{% trans 'Problem was not saved.' %}")
     else:
-        print('{% trans "The problem is not correctly formulated." %}')
+        print("{% trans 'The problem is not correctly formulated.' %}")
