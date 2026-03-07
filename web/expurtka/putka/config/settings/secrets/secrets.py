d69af41f876c572ecc1a02669b95d82367180602
Bor Brudar <bor.brudar@gmail.com>
2026-03-07 16:55:11 +0100

    updates reqs & readme

---
+++ web/expurtka/putka/config/settings/secrets/secrets.py
@@ -0,0 +1,12 @@
+# Sample secrets file. Rename and import in the appropriate settings file.
+SECRET_KEY = 'test'
+RECAPTCHA_PRIVATE_KEY = ''
+RECAPTCHA_PUBLIC_KEY = ''
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 0
+EMAIL_USE_TLS = True
+DATABASE_NAME = 'test'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
