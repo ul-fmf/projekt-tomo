4a91696315238a3c9668684c7cbc957a854420dd
Jakob Schrader <jakob.schrader@guest.arnes.si>
2026-03-07 18:49:17 +0100

    Add exppurtka app and command

---
+++ web/exppurtka/management/commands/exppurtka.py
@@ -0,0 +1,29 @@
+"""
+Export to Putka
+"""
+
+import json
+
+from django.core.management import BaseCommand
+from exppurtka.export import problems
+
+
+class Command(BaseCommand):
+    help = """JSON dumps the tomo models for putka import"""
+
+    def handle(self, *args, **options):
+        self.stdout.write("creating dict to dump...")
+
+        obj = {}
+        obj['tasks'] = problems.get_problems()
+
+        self.stdout.write("dumping JSON...")
+
+        with open(options['outfile'], 'w') as f:
+            json.dump(obj, f)
+
+        self.stdout.write("Done!")
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument('outfile', help="The path to which to export the JSON to")
--- web/web/settings/common.py
+++ web/web/settings/common.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "courses",
     "taggit",
     "social_django",
+    "exppurtka",
 ]
 
 MIDDLEWARE = [
